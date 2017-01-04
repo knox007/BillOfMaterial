32a82dc2f2b80f449366d366fe1897c25a5d4bc2
Vlastimil Sadilek <vlastoun@gmail.com>
2017-01-04 17:24:40 +0100

    init

---
+++ BillOfMaterial/MainWND.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace BillOfMaterial {
+    partial class MainWND {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridProfiles = new System.Windows.Forms.DataGridView();
+            this.numberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.weightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.profileBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.TotalWeight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProfiles)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profileBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridProfiles
+            // 
+            this.dataGridProfiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridProfiles.AutoGenerateColumns = false;
+            this.dataGridProfiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProfiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numberDataGridViewTextBoxColumn,
+            this.weightDataGridViewTextBoxColumn,
+            this.TotalWeight});
+            this.dataGridProfiles.DataSource = this.profileBindingSource;
+            this.dataGridProfiles.Location = new System.Drawing.Point(12, 12);
+            this.dataGridProfiles.Name = "dataGridProfiles";
+            this.dataGridProfiles.Size = new System.Drawing.Size(456, 329);
+            this.dataGridProfiles.TabIndex = 0;
+            // 
+            // numberDataGridViewTextBoxColumn
+            // 
+            this.numberDataGridViewTextBoxColumn.DataPropertyName = "Number";
+            this.numberDataGridViewTextBoxColumn.HeaderText = "Number";
+            this.numberDataGridViewTextBoxColumn.Name = "numberDataGridViewTextBoxColumn";
+            // 
+            // weightDataGridViewTextBoxColumn
+            // 
+            this.weightDataGridViewTextBoxColumn.DataPropertyName = "Weight";
+            this.weightDataGridViewTextBoxColumn.HeaderText = "Weight";
+            this.weightDataGridViewTextBoxColumn.Name = "weightDataGridViewTextBoxColumn";
+            // 
+            // profileBindingSource
+            // 
+            this.profileBindingSource.DataSource = typeof(Profile);
+            // 
+            // TotalWeight
+            // 
+            this.TotalWeight.DataPropertyName = "TotalWeight";
+            this.TotalWeight.HeaderText = "TotalWeight";
+            this.TotalWeight.Name = "TotalWeight";
+            this.TotalWeight.ReadOnly = true;
+            // 
+            // MainWND
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 353);
+            this.Controls.Add(this.dataGridProfiles);
+            this.Name = "MainWND";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProfiles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profileBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridProfiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource profileBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalWeight;
+    }
+}
+
---
+++ BillOfMaterial/Profile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Profile {
+    public Profile() { }
+
+    private int _number;
+    private double _weight;
+    private double _totalWeight;
+
+    public int Number {
+        get { return _number; }
+        set { _number = value; calculateTotals(); }
+    }
+    public double Weight {
+        get { return _weight; }
+        set { _weight = value; calculateTotals(); }
+    }
+    public double TotalWeight {
+        get { return _totalWeight; }
+    }
+    
+    private void calculateTotals() {
+        this._totalWeight = this.Number * this._weight;
+    }
+
+}
+
